f1fce4d8291fd41c8ab502cd5936f8f717f6824e
Mahbubur Rahman <mahbubur001@gmail.com>
2019-02-21 23:09:47 +0600

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+const min = Mix.inProduction() ? '.min' : '';
+
+if (!Mix.inProduction()) {
+    mix.sourceMaps();
+}
+mix.babel(`src/js/mobilemenu.js`, `mobilemenu${min}.js`);
+// mix.sass(`src/scss/admin.scss`, `assets/css/admin${min}.css`);
+
